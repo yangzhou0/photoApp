71f0dddb30341da74a7afe740008464ab628fbe5
Yang Zhou <yzhou3991@gmail.com>
2020-04-22 20:53:57 -0400

    import httpClient service and initiate it in photo.service

--- photoApp/src/app/app.module.ts
+++ photoApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhotosComponent } from './photos/photos.component';
@@ -12,7 +12,8 @@ import { PhotosComponent } from './photos/photos.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- photoApp/src/app/core/photo.service.ts
+++ photoApp/src/app/core/photo.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { PHOTOS } from '../../assets/photos';
 import { IPhoto } from '../shared/interface';
-
+import {HttpClient} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
@@ -15,5 +15,5 @@ export class PhotoService {
   return PHOTOS.find((el) => {return el._id == id});
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
