94d4ac41fe7bc6dd980cf0890ac731255135b4e6
Yang Zhou <yzhou3991@gmail.com>
2020-04-29 01:31:22 -0400

    add two-way binding to edit form

--- photoApp/src/app/photos/photo/edit-photo-form/edit-photo-form.component.ts
+++ photoApp/src/app/photos/photo/edit-photo-form/edit-photo-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'edit-photo-form',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-photo-form.component.css']
 })
 export class EditPhotoFormComponent implements OnInit {
-
+  @Input() photo;
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
