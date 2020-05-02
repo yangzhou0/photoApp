8156d067f52b4890464350009b5bd52a5ece533e
Yang Zhou <yzhou3991@gmail.com>
2020-05-02 15:50:07 -0400

    add file input and file input handler fuinction

--- photoApp/src/app/photos/create-bar/create-bar.component.ts
+++ photoApp/src/app/photos/create-bar/create-bar.component.ts
@@ -9,10 +9,17 @@ import {PhotoService} from '../../core/photo.service';
 export class CreateBarComponent implements OnInit {
   photo = {
     description: 'enter some dope description',
-    hashtag: 'enter some cool # '
+    hashtag: 'enter some cool # ',
+    photo: File
   }
-  onSubmit(data){
-    console.log(data);
+  onSubmit(){
+
+  }
+
+  handleFileInput(target){
+    let photoFile = target.files[0]; // select the single image we choose
+    this.photo.photo = photoFile;
+    console.log(this.photo);
   }
   constructor(private photoService: PhotoService) { }
 
