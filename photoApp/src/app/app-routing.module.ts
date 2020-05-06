2d5b8804cd01612d857b8402993e5d5917794b16
Yang Zhou <yzhou3991@gmail.com>
2020-05-06 19:40:22 -0400

    add login into routing

--- photoApp/src/app/app-routing.module.ts
+++ photoApp/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import {PhotosComponent} from './photos/photos.component';
 import {PhotoComponent} from './photos/photo/photo.component';
+import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
   //set up routing
   { path: 'photos/:id', component: PhotoComponent},
   { path: 'photos', component: PhotosComponent},
+  { path: 'login', component: LoginComponent},
   { path: '', redirectTo: '/photos', pathMatch: 'full'}, //www.xx.com would direct to photos component
   { path: '**', pathMatch: 'full', redirectTo: '/photos' } // wildcard, redirect to homepage if no url matched
 ]; // sets up routes constant where you define your routes
