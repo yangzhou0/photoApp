19ca8c31ead2e6eda3282629912d9455691f916f
Yang Zhou <yzhou3991@gmail.com>
2020-05-18 23:53:01 -0400

    add register button

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import {PhotosComponent} from './photos/photos.component';
 import {PhotoComponent} from './photos/photo/photo.component';
 import { PhotosListComponent } from './photos/photos-list/photos-list.component';
+import { RegisterComponent } from './register/register.component';
 
 import {LoginComponent} from './login/login.component';
 
@@ -21,6 +22,7 @@ const routes: Routes = [
       }
     ]},
   { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
   { path: '', redirectTo: '/photos', pathMatch: 'full'}, //www.xx.com would direct to photos component
   { path: '**', pathMatch: 'full', redirectTo: '/photos' } // wildcard, redirect to homepage if no url matched
 ]; // sets up routes constant where you define your routes
