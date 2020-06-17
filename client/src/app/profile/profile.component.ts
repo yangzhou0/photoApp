ce8a717d601f452b151d346b6e91a6a1bcd59393
Yang Zhou <yzhou3991@gmail.com>
2020-06-17 01:34:58 -0400

    load user info at profile component

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AuthService} from '../core/auth.service'
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  user
+  getCurrentUser(): void {
+    //grab the localStorage user to assign it to variable
+    this.user = this.authService.getUser();
+  }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.getCurrentUser()
   }
 
 }
